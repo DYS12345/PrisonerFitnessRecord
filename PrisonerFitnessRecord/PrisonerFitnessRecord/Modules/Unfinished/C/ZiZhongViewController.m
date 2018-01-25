3d66fd380d5b2b375816312567de7f8d8a295090
dong <609042238@qq.com>
2018-01-25 18:13:27 +0800

    3

--- PrisonerFitnessRecord/PrisonerFitnessRecord/Modules/Unfinished/C/ZiZhongViewController.m
+++ PrisonerFitnessRecord/PrisonerFitnessRecord/Modules/Unfinished/C/ZiZhongViewController.m
@@ -10,12 +10,14 @@
 #import "ZiZhongTableViewCell.h"
 #import "UIView+Size.h"
 #import "OtherMacro.h"
+#import "ZiZhongJiShuTableViewCell.h"
 
 @interface ZiZhongViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray *imageAry;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *selectViewTop;
+@property (nonatomic, assign) NSInteger flagN;
 
 @end
 
@@ -31,36 +33,75 @@
     self.navigationItem.rightBarButtonItem = selectBtnItem;
     
     [self.tableView registerNib:[UINib nibWithNibName:@"ZiZhongTableViewCell" bundle:nil] forCellReuseIdentifier:@"ZiZhongTableViewCell"];
+    [self.tableView registerNib:[UINib nibWithNibName:@"ZiZhongJiShuTableViewCell" bundle:nil] forCellReuseIdentifier:@"ZiZhongJiShuTableViewCell"];
     
     NSMutableArray *imageAry = [NSMutableArray array];
     for (int i = 1; i<=5; i++) {
         [imageAry addObject:[NSString stringWithFormat:@"ziZhong%d", i]];
     }
     self.imageAry = imageAry;
+    self.flagN = 0;
+}
+
+- (IBAction)firstBtn:(id)sender {
+    self.flagN = 0;
+    [self.tableView reloadData];
+}
+
+- (IBAction)secondbtn:(id)sender {
+    self.flagN = 1;
+    [self.tableView reloadData];
+}
+
+- (IBAction)thirdBtn:(id)sender {
+    self.flagN = 2;
+    [self.tableView reloadData];
+}
+
+- (IBAction)fourBtn:(id)sender {
+    self.flagN = 4;
+    [self.tableView reloadData];
+}
+
+- (IBAction)fuBtn:(id)sender {
+    self.flagN = 3;
+    [self.tableView reloadData];
+}
+
+- (IBAction)selectViewCancel:(id)sender {
+    [UIView animateWithDuration:0.4 animations:^{
+        self.selectViewTop.constant = 600;
+        [self.view layoutIfNeeded];
+    }];
 }
 
 -(void)select{
-    [UIView animateWithDuration:0.25 animations:^{
+    [UIView animateWithDuration:0.4 animations:^{
         self.selectViewTop.constant = 0; // 600
         [self.view layoutIfNeeded];
     }];
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 1;
+    return 2;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    ZiZhongTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ZiZhongTableViewCell"];
-    cell.contentImageView.image = [UIImage imageNamed:self.imageAry[indexPath.row]];
-    return cell;
+    if (indexPath.item == 1) {
+        ZiZhongJiShuTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ZiZhongJiShuTableViewCell"];
+        return cell;
+    } else {
+        ZiZhongTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ZiZhongTableViewCell"];
+        cell.contentImageView.image = [UIImage imageNamed:self.imageAry[self.flagN]];
+        return cell;
+    }
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     if (indexPath.row == 0) {
         return 10/7.0 * SCREEN_WIDTH;
     }
-    return 0;
+    return 50;
 }
 
 @end
---
+++ PrisonerFitnessRecord/PrisonerFitnessRecord/Modules/Unfinished/V/ZiZhongJiShuTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ZiZhongJiShuTableViewCell.m
+//  PrisonerFitnessRecord
+//
+//  Created by dong on 2018/1/25.
+//  Copyright © 2018年 董永胜. All rights reserved.
+//
+
+#import "ZiZhongJiShuTableViewCell.h"
+
+@implementation ZiZhongJiShuTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
